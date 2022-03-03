a9ed7091d15eab546aa9f21b91e450dc041f4029
devopsPRO27 <96721542+devopsPRO27@users.noreply.github.com>
2022-03-03 19:58:48 +0200

    Update sql222.sql

--- sql222.sql
+++ sql222.sql
@@ -24,6 +24,13 @@ CREATE TABLE Students(
 );
 
 
-
+INSERT INTO Students (name,age,address_City,vip)
+VALUES
+("batman",39,"ashdod","no"),
+("deadpool",57,"naharia","YES"),
+("superman",29,"beersheva","no"),
+("wonder woman",34,"mizraa","no"),
+("green arrow",19,"telaviv","YES"),
+("spiderman",69,"naura","no")
 
 
